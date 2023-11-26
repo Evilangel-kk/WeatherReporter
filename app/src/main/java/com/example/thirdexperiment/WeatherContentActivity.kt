7d380d96294ad721cbd8c2fd0919c0d689a82a16
Evilangel <2472047312@qq.com>
2023-11-26 10:15:32 +0800

    分享

--- app/src/main/java/com/example/thirdexperiment/WeatherContentActivity.kt
+++ app/src/main/java/com/example/thirdexperiment/WeatherContentActivity.kt
@@ -2,12 +2,14 @@ package com.example.thirdexperiment
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.thirdexperiment.databinding.ActivityWeatherContentBinding
+
 
 /*
 * 天气细节Activity*/
@@ -38,13 +40,51 @@ class WeatherContentActivity : AppCompatActivity() {
         }
     }
 
+
+    // 添加菜单栏
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        if(menu!=null){
+            menuInflater.inflate(R.menu.menu_share,menu)
+            menuInflater.inflate(R.menu.menu_optionmenu,menu)
+        }
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
                 finish() // back button
                 return true
             }
+            R.id.mail_share->{
+                sendEML(getWeatherInfo())
+            }
+            R.id.message_share->{
+                sendSMS(getWeatherInfo())
+            }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun sendSMS(message: String) {
+        var smsToUri:Uri = Uri.parse("smsto:");
+        var intent = Intent(Intent.ACTION_SENDTO, smsToUri);
+        intent.putExtra("sms_body", message);
+        startActivity(intent);
+    }
+
+    private fun sendEML(message: String){
+        var uri = Uri.parse("mailto:")
+        var email = "2472047312@qq.com"
+        var intent = Intent(Intent.ACTION_SENDTO, uri)
+        intent.putExtra(Intent.EXTRA_CC, email) // 抄送人
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Weather Update"); // 主题
+        intent.putExtra(Intent.EXTRA_TEXT, message); // 正文
+        startActivity(Intent.createChooser(intent, "请选择邮件类应用"));
+    }
+
+    private fun getWeatherInfo():String{
+        var w = WeatherList.weather[0];
+        return w.dayOfWeek+"\nMaxTemp: "+w.max_temp+"\nMinTemp: "+w.min_temp+"\n"+w.textDay
     }
 }
